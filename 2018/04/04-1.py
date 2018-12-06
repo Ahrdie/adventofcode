8181ec04f2a4e19a398e41fad4bdd4df2f86e00f
Robert Deppe <robert.deppe@posteo.de>
2018-12-06 21:40:50 +0100

    [Add] data, testdata and solution for day four part one

---
+++ 2018/04/04-1.py
@@ -0,0 +1,70 @@
+sourceFile = open("source.txt","r")
+testData = open("testdata.txt","r")
+
+allGuardEvents = []
+
+for sourceLine in sourceFile:
+    timestamp, message = sourceLine.replace("\n","").split("]")
+    
+    date, time = timestamp.replace("[","").split(" ")
+    year, month, day = date.split("-")
+    hour, minute = time.split(":")
+    
+    newEvent = {
+        "year" : int(year),
+        "month" : int(month),
+        "day" : int(day),
+        "hour" : int(hour),
+        "minute" : int(minute),
+        "message" : message[1:]
+    }
+    allGuardEvents.append(newEvent)
+
+allGuardEvents = sorted(allGuardEvents, key=lambda k: k["minute"])
+allGuardEvents = sorted(allGuardEvents, key=lambda k: k["hour"])
+allGuardEvents = sorted(allGuardEvents, key=lambda k: k["day"])
+allGuardEvents = sorted(allGuardEvents, key=lambda k: k["month"])
+allGuardEvents = sorted(allGuardEvents, key=lambda k: k["year"])
+
+guardTimes = {}
+guardId = ""
+fellAsleep = 0
+
+for event in allGuardEvents:
+    
+    if event["message"].find("Guard") == 0:
+        g, guardId, b, s = event["message"].split(" ")
+    
+    elif event["message"].find("falls") == 0:
+        fellAsleep = event["minute"]
+    
+    elif guardId in guardTimes:
+        pattern = guardTimes[guardId]
+        for m in range(int(fellAsleep),int(event["minute"])):
+            pattern[m] = pattern[m] +1
+        guardTimes[guardId] = pattern
+
+    else:
+        pattern = [0] * 60
+        for m in range(int(fellAsleep),int(event["minute"])):
+            pattern[m] = pattern[m] +1
+        guardTimes[guardId] = pattern
+
+maxSleepSum = 0
+for guard in guardTimes:
+    minuteSum = 0
+    for m in guardTimes[guard]:
+        minuteSum = minuteSum + m
+    if minuteSum > maxSleepSum:
+        guardId = guard
+        maxSleepSum = minuteSum
+
+maxMinute = 0;
+timesAsleep = 0;
+
+for minute in guardTimes[guardId]:
+    if minute > timesAsleep:
+        maxMinute = guardTimes[guardId].index(minute)
+        timesAsleep = minute
+
+print "Elf " + str(guardId) + " sleeps most. Most often at 0:" + str(maxMinute) + ". Code: " + str(int(guardId[1:]) * maxMinute)
