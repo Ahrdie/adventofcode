49202cca79b2d0e613a5c93a6b72f3842e3bfddc
Robert Deppe <robert.deppe@posteo.de>
2018-12-16 17:19:53 +0100

    [Add] solution for day nine part two

---
+++ 2018/09/09-2.py
@@ -0,0 +1,21 @@
+# Not my solution.
+# Taken from @marcusandrews:
+# https://www.reddit.com/r/adventofcode/comments/a4i97s/2018_day_9_solutions/ebepyc7
+
+from collections import deque, defaultdict
+
+def play_game (max_players, last_marble) :
+    scores = defaultdict(int)
+    circle = deque([0])
+
+    for marble in range(1, last_marble +1):
+        if marble % 23 == 0:
+            circle.rotate(7)
+            scores[marble % max_players] += marble + circle.pop()
+            circle.rotate(-1)
+        else:
+            circle.rotate(-1)
+            circle.append(marble)
+    return max(scores.values()) if scores else 0
+
+print play_game(464, 7091800)
